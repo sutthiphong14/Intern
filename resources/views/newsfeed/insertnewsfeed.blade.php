822cf85d5f4ffc175262b84527786564972db666
weerapat <weerapat.so@rmuti.ac.th>
2024-11-22 10:34:12 +0700

    fix insert

--- resources/views/newsfeed/insertnewsfeed.blade.php
+++ resources/views/newsfeed/insertnewsfeed.blade.php
@@ -60,12 +60,7 @@
                                     </td>
                                     
                                     <td>
-                                        <div class="form-group">
-                                            <div class="custom-file">
-                                                <input type="file" class="custom-file-input" id="customFile">
-                                                <label class="custom-file-label" for="customFile">Choose file</label>
-                                            </div>
-                                        </div>
+                                    <input class="form-control" type="text" placeholder="Linkfile">
                                     </td>
                                 </tr>
                             </tbody>
