03b6fabcc3979c0fd24a0ecd7036b72643851d54
sutthiphong <sutthipong25451@gmail.com>
2025-01-09 11:26:35 +0700

    new_2_decimal

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -433,7 +433,7 @@ class ReportController extends Controller
         // หารผลรวมด้วยจำนวนเดือนที่มี
         $data1 = [];
         foreach ($combinedData as $label => $sum) {
-            $data1[$label] = number_format($sum / $countOfMonths, 2);  // แปลงผลลัพธ์เป็นทศนิยม 2 ตำแหน่ง
+            $data1[$label] = round($sum / $countOfMonths, 2);  // ปัดทศนิยม 2 ตำแหน่ง
         }
         $latestYear = $latestMonthData->first()->year ?? ''; // เลือกปีจากข้อมูลล่าสุดใน Collection
         // คืนค่าผลลัพธ์ไปยัง view พร้อมกับทั้งสองตัวแปร
