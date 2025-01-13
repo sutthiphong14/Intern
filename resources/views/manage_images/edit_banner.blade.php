db3d194f30db92c283ebb4bab9e61cf0457ad9c2
weerapat <weerapat.so@rmuti.ac.th>
2025-01-13 14:05:07 +0700

    up
    า

--- resources/views/manage_images/edit_banner.blade.php
+++ resources/views/manage_images/edit_banner.blade.php
@@ -7,16 +7,6 @@
 @endsection
 @section('css')
 <style>
-    .content-wrapper .card {
-        overflow: hidden;
-        /* ซ่อนเนื้อหาที่เกินขอบ */
-    }
-
-    #carouselExampleIndicators {
-        width: 100%;
-        /* ให้ Carousel มีความกว้างเต็มของ Card */
-    }
-
     .carousel-control-prev,
     .carousel-control-next {
         width: 5%;
@@ -65,100 +55,109 @@
 @endsection
 @section('content')
 <div class="content-wrapper">
-    <div class="card mb-1">
-    <div class="d-flex justify-content-between align-items-center gap-2">
+    <div class="card">
+        <div class="d-flex justify-content-between align-items-center gap-2">
             <!-- หัวข้อ -->
-            <h5 class="card-header text-dark">
+            <h3 class="card-header text-dark">
                 จัดการ Slideshow
-            </h5>
+            </h3>
             <div class="d-flex align-items-center gap-2">
-                <button type="button" class="btn bg-dark btn-fixed-size" data-toggle="modal" data-target="#exportModal">
-                    <i class="fas fa-file-export"></i> Export
-                </button>
 
-                <button type="button" class="btn btn-dark me-4" data-bs-toggle="modal" data-bs-target="#modalScrollable">
+                <button type="button" class="btn btn-dark me-4" data-bs-toggle="modal"
+                    data-bs-target="#modalScrollable">
                     <i class="fas fa-question-circle"></i>
                 </button>
             </div>
         </div>
         <div class="card-body">
-        <div class="slide">
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" data-interval="500">
-
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="{{ asset('/img/banner_images/1.png') }}" class="d-block w-100 rounded" alt="Banner 1">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{ asset('/img/banner_images/2.png') }}" class="d-block w-100 rounded" alt="Banner 2">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{ asset('/img/banner_images/3.png') }}" class="d-block w-100 rounded" alt="Banner 3">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{ asset('/img/banner_images/4.png') }}" class="d-block w-100 rounded" alt="Banner 4">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{ asset('/img/banner_images/5.png') }}" class="d-block w-100 rounded" alt="Banner 5">
-                    </div>
-                </div>
-
+        <div class="slide"> 
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" data-bs-pause="true" data-bs-interval="false">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('/img/banner_images/1.png') }}" class="d-block w-100 rounded" alt="Banner 1">
             </div>
-
-        </div>
-
-
-
-    </div>
-
-
-
-
-
-
-
-
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="button-container d-flex flex-nowrap justify-content-start gap-2 py-3 overflow-auto">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                        aria-label="Slide 1" class="p-0 border-0">
-                        <img src="{{ asset('/img/banner_images/1.png') }}" style="width: 160px; height: 40px;"
-                            class="rounded" alt="Banner 1">
-                    </button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                        aria-label="Slide 2" class="p-0 border-0">
-                        <img src="{{ asset('/img/banner_images/2.png') }}" style="width: 160px; height: 40px;"
-                            class="rounded" alt="Banner 2">
-                    </button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                        aria-label="Slide 3" class="p-0 border-0">
-                        <img src="{{ asset('/img/banner_images/3.png') }}" style="width: 160px; height: 40px;"
-                            class="rounded" alt="Banner 3">
-                    </button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
-                        aria-label="Slide 4" class="p-0 border-0">
-                        <img src="{{ asset('/img/banner_images/4.png') }}" style="width: 160px; height: 40px;"
-                            class="rounded" alt="Banner 4">
-                    </button>
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
-                        aria-label="Slide 5" class="p-0 border-0">
-                        <img src="{{ asset('/img/banner_images/5.png') }}" style="width: 160px; height: 40px;"
-                            class="rounded" alt="Banner 5">
-                    </button>
-                    <button type="button" style="width: 160px; height: 40px;" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5"
-                        aria-label="Slide 6" class="p-0 border-0 bg-dark" >
-                        +
-                    </button>
-                </div>
+            <div class="carousel-item">
+                <img src="{{ asset('/img/banner_images/2.png') }}" class="d-block w-100 rounded" alt="Banner 2">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('/img/banner_images/3.png') }}" class="d-block w-100 rounded" alt="Banner 3">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('/img/banner_images/4.png') }}" class="d-block w-100 rounded" alt="Banner 4">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('/img/banner_images/5.png') }}" class="d-block w-100 rounded" alt="Banner 5">
             </div>
         </div>
     </div>
+</div>
+
+
+
+
+        
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="button-container d-flex flex-nowrap justify-content-start gap-2 py-3 overflow-auto">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                            aria-label="Slide 1" class="p-0 border-0">
+                            <img src="{{ asset('/img/banner_images/1.png') }}" style="width: 160px; height: 40px;"
+                                class="rounded" alt="Banner 1">
+                        </button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                            aria-label="Slide 2" class="p-0 border-0">
+                            <img src="{{ asset('/img/banner_images/2.png') }}" style="width: 160px; height: 40px;"
+                                class="rounded" alt="Banner 2">
+                        </button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                            aria-label="Slide 3" class="p-0 border-0">
+                            <img src="{{ asset('/img/banner_images/3.png') }}" style="width: 160px; height: 40px;"
+                                class="rounded" alt="Banner 3">
+                        </button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+                            aria-label="Slide 4" class="p-0 border-0">
+                            <img src="{{ asset('/img/banner_images/4.png') }}" style="width: 160px; height: 40px;"
+                                class="rounded" alt="Banner 4">
+                        </button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
+                            aria-label="Slide 5" class="p-0 border-0">
+                            <img src="{{ asset('/img/banner_images/5.png') }}" style="width: 160px; height: 40px;"
+                                class="rounded" alt="Banner 5">
+                        </button>
+                        <button type="button" style="width: 160px; height: 40px;"
+                            data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"
+                            class="p-0 border-0 bg-dark">
+                            +
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="mb-3">
+            <label for="" class="form-label text-dark">เลือกไฟล์รูปภาพ</label>
+            <input class="form-control" type="file" id="import_file" name="import_file">
+        </div>
+
+        <div class="mb-3">
+            <label for="" class="form-label text-dark">เชื่อม Link ดาวโหลด</label>
+            <input type="text" class="form-control" id="" name="" placeholder="กรอกชื่อ-นามสกุล" required
+                value="">
+        </div>
+
+       
+
+
+
+
+
     </div>
 
 </div>
-
+</div>
 
 
 @endsection
