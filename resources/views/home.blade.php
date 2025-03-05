bec9121c35de263464c7d6fc6f02764eeadb078a
Ton2511 <tonnum251139@gmail.com>
2025-03-05 10:31:15 +0700

    s

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -586,6 +586,33 @@
     });
 </script>
 
+<script>
+    // SweetAlert function for unauthorized access
+    function showUnauthorizedAlert() {
+    Swal.fire({
+      icon: 'warning',
+      title: 'ไม่มีสิทธิเข้าถึง',
+      text: 'กรุณาเข้าสู่ระบบเพื่อใช้งานฟังก์ชันนี้',
+      confirmButtonText: 'ตกลง',
+    });
+    }
+  </script>
+  <!-- เพิ่ม SweetAlert2 CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+  <script>
+    document.addEventListener("DOMContentLoaded", function () {
+    @if(session('success'))
+    Swal.fire({
+      title: "สำเร็จ!",
+      text: "{{ session('success') }}",
+      icon: "success",
+      confirmButtonText: "ตกลง"
+    });
+  @endif
+    });
+  </script>
+
 
   
 @endsection
